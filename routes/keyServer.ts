100a9c1ced19ae02be8ced88450bd9cb104f1cf2
giannisgkountras <gkountrasioannis@gmail.com>
2024-12-17 22:02:48 +0200

    fix path in keyServer

--- routes/keyServer.ts
+++ routes/keyServer.ts
@@ -3,18 +3,18 @@
  * SPDX-License-Identifier: MIT
  */
 
-import path = require('path')
-import { type Request, type Response, type NextFunction } from 'express'
+import path = require("path");
+import { type Request, type Response, type NextFunction } from "express";
 
-module.exports = function serveKeyFiles () {
+module.exports = function serveKeyFiles() {
   return ({ params }: Request, res: Response, next: NextFunction) => {
-    const file = params.file
+    const file = params.file;
 
-    if (!file.includes('/')) {
-      res.sendFile(path.resolve('encryptionkeys/', file))
+    if (!file.includes("/")) {
+      res.sendFile(path.resolve("encryptionkeys/", path.basename(file)));
     } else {
-      res.status(403)
-      next(new Error('File names cannot contain forward slashes!'))
+      res.status(403);
+      next(new Error("File names cannot contain forward slashes!"));
     }
-  }
-}
+  };
+};
