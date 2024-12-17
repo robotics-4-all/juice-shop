a190740210df6df5a27c579f2cd772515eb8fd38
bekouvas <79990762+bekouvas@users.noreply.github.com>
2024-12-17 22:39:30 +0200

    Violations fixing
    fixing violations in lib\antiCheat.ts and /cypress.config.ts. (Usage of 'any')

--- lib/antiCheat.ts
+++ lib/antiCheat.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import config from 'config'
 import colors from 'colors/safe'
 import { retrieveCodeSnippet } from '../routes/vulnCodeSnippet'
@@ -24,9 +19,22 @@ const coupledChallenges = { // TODO prevent also near-identical challenges (e.g.
 }
 const trivialChallenges = ['errorHandlingChallenge', 'privacyPolicyChallenge', 'closeNotificationsChallenge']
 
-const solves: Array<{ challenge: any, phase: string, timestamp: Date, cheatScore: number }> = [{ challenge: {}, phase: 'server start', timestamp: new Date(), cheatScore: 0 }] // seed with server start timestamp
+interface Solve {
+  challenge: Challenge,
+  phase: string,
+  timestamp: Date,
+  cheatScore: number
+}
 
-const preSolveInteractions: Array<{ challengeKey: any, urlFragments: string[], interactions: boolean[] }> = [
+interface PreSolveInteraction {
+  challengeKey: string,
+  urlFragments: string[],
+  interactions: boolean[]
+}
+
+const solves: Solve[] = [{ challenge: {} as Challenge, phase: 'server start', timestamp: new Date(), cheatScore: 0 }] // seed with server start timestamp
+
+const preSolveInteractions: PreSolveInteraction[] = [
   { challengeKey: 'missingEncodingChallenge', urlFragments: ['/assets/public/images/uploads/%F0%9F%98%BC-'], interactions: [false] },
   { challengeKey: 'directoryListingChallenge', urlFragments: ['/ftp'], interactions: [false] },
   { challengeKey: 'easterEggLevelOneChallenge', urlFragments: ['/ftp', '/ftp/eastere.gg'], interactions: [false, false] },
