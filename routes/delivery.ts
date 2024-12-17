84cba0c03deab117766980e060e8f774acc652cc
dimitrakolitsa <dimitrageoria@gmail.com>
2024-12-17 20:37:37 +0200

    removed 2 unused variables in delivery.ts

--- routes/delivery.ts
+++ routes/delivery.ts
@@ -3,16 +3,16 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { type Request, type Response, type NextFunction } from 'express'
-import { DeliveryModel } from '../models/delivery'
+import { type Request, type Response, type NextFunction } from "express";
+import { DeliveryModel } from "../models/delivery";
 
-const security = require('../lib/insecurity')
+const security = require("../lib/insecurity");
 
-module.exports.getDeliveryMethods = function getDeliveryMethods () {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const methods = await DeliveryModel.findAll()
+module.exports.getDeliveryMethods = function getDeliveryMethods() {
+  return async (req: Request, res: Response, _: NextFunction) => {
+    const methods = await DeliveryModel.findAll();
     if (methods) {
-      const sendMethods = []
+      const sendMethods = [];
       for (const method of methods) {
         sendMethods.push({
           id: method.id,
@@ -20,18 +20,18 @@ module.exports.getDeliveryMethods = function getDeliveryMethods () {
           price: security.isDeluxe(req) ? method.deluxePrice : method.price,
           eta: method.eta,
           icon: method.icon
-        })
+        });
       }
-      res.status(200).json({ status: 'success', data: sendMethods })
+      res.status(200).json({ status: "success", data: sendMethods });
     } else {
-      res.status(400).json({ status: 'error' })
+      res.status(400).json({ status: "error" });
     }
-  }
-}
+  };
+};
 
-module.exports.getDeliveryMethod = function getDeliveryMethod () {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    const method = await DeliveryModel.findOne({ where: { id: req.params.id } })
+module.exports.getDeliveryMethod = function getDeliveryMethod() {
+  return async (req: Request, res: Response, _: NextFunction) => {
+    const method = await DeliveryModel.findOne({ where: { id: req.params.id } });
     if (method != null) {
       const sendMethod = {
         id: method.id,
@@ -39,10 +39,10 @@ module.exports.getDeliveryMethod = function getDeliveryMethod () {
         price: security.isDeluxe(req) ? method.deluxePrice : method.price,
         eta: method.eta,
         icon: method.icon
-      }
-      res.status(200).json({ status: 'success', data: sendMethod })
+      };
+      res.status(200).json({ status: "success", data: sendMethod });
     } else {
-      res.status(400).json({ status: 'error' })
+      res.status(400).json({ status: "error" });
     }
-  }
-}
+  };
+};
