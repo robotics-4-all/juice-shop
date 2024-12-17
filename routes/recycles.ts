59aef233d9489e16daf68e68832a77dffc03f976
giannisgkountras <gkountrasioannis@gmail.com>
2024-12-18 01:38:25 +0200

    Remove unused var in recycles.ts

--- routes/recycles.ts
+++ routes/recycles.ts
@@ -15,7 +15,7 @@ exports.getRecycleItem = () => (req: Request, res: Response) => {
     }
   }).then((Recycle) => {
     return res.send(utils.queryResultToJson(Recycle))
-  }).catch((_: unknown) => {
+  }).catch(() => {
     return res.send('Error fetching recycled items. Please try again')
   })
 }
