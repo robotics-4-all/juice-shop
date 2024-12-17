00d2667211a3751557644edc29780e6fee9a4166
giannisgkountras <gkountrasioannis@gmail.com>
2024-12-17 22:00:01 +0200

    fixed path in quarantine server

--- routes/quarantineServer.ts
+++ routes/quarantineServer.ts
@@ -3,18 +3,18 @@
  * SPDX-License-Identifier: MIT
  */
 
-import path = require('path')
-import { type Request, type Response, type NextFunction } from 'express'
+import path = require("path");
+import { type Request, type Response, type NextFunction } from "express";
 
-module.exports = function serveQuarantineFiles () {
+module.exports = function serveQuarantineFiles() {
   return ({ params, query }: Request, res: Response, next: NextFunction) => {
-    const file = params.file
+    const file = params.file;
 
-    if (!file.includes('/')) {
-      res.sendFile(path.resolve('ftp/quarantine/', file))
+    if (!file.includes("/")) {
+      res.sendFile(path.resolve("ftp/quarantine/", path.basename(file)));
     } else {
-      res.status(403)
-      next(new Error('File names cannot contain forward slashes!'))
+      res.status(403);
+      next(new Error("File names cannot contain forward slashes!"));
     }
-  }
-}
+  };
+};
