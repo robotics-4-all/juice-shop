e0bac49a37518c97f6f8694539362e464e123b00
chrisdardas <85883633+chrisdardas@users.noreply.github.com>
2024-12-18 12:38:43 +0200

    Minor security fixes

--- routes/userProfile.ts
+++ routes/userProfile.ts
@@ -37,7 +37,7 @@ module.exports = function getUserProfile () {
               // username = eval(code) // eslint-disable-line no-eval
               try {
                 const safeCode = code.replace(/[^a-zA-Z0-9_]/g, '')
-                username = eval(safeCode) // eslint-disable-line no-eval
+                username = safeCode
               } catch (error) {
                 console.error('Error evaluating code:', error)
                 username = undefined // or handle the error appropriately
@@ -63,7 +63,7 @@ module.exports = function getUserProfile () {
           template = template.replace(/_logo_/g, utils.extractFilename(config.get('application.logo')))
           // Sanitize and escape user input
           const sanitizedUser = {
-            ...req.user,
+            ...user,
             username: xss((req.user as any)?.username)
             // Sanitize other user properties as needed
           }
