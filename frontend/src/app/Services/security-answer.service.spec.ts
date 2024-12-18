59802b1b1d990035c81c05067113a85706fa3573
chrisvobi <christoforidis02@gmail.com>
2024-12-18 09:03:16 +0200

    removed 'any' type

--- frontend/src/app/Services/security-answer.service.spec.ts
+++ frontend/src/app/Services/security-answer.service.spec.ts
@@ -8,6 +8,10 @@ import { fakeAsync, inject, TestBed, tick } from '@angular/core/testing'
 
 import { SecurityAnswerService } from './security-answer.service'
 
+interface ApplicationVersionResponse {
+  version: string;
+}
+
 describe('SecurityAnswerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,15 +26,15 @@ describe('SecurityAnswerService', () => {
 
   it('should create feedback directly via the rest api', inject([SecurityAnswerService, HttpTestingController],
     fakeAsync((service: SecurityAnswerService, httpMock: HttpTestingController) => {
-      let res: any
-      service.save(null).subscribe((data) => (res = data))
+      let res: ApplicationVersionResponse | undefined
+      service.save(null).subscribe((data: ApplicationVersionResponse) => (res = data))
       const req = httpMock.expectOne('http://localhost:3000/api/SecurityAnswers/')
       req.flush({ data: 'apiResponse' })
 
       tick()
       expect(req.request.method).toBe('POST')
       expect(req.request.body).toBeFalsy()
-      expect(res).toBe('apiResponse')
+      expect(res?.version).toBe('apiResponse')
       httpMock.verify()
     })
   ))
